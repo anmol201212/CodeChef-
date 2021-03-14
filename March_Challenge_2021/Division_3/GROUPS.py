5ab48b280e144bf9ac25992c9a29592d9677390b
Anmol Chopra <54865022+anmol201212@users.noreply.github.com>
2021-03-14 16:25:59 +0530

    Add files via upload

---
+++ March_Challenge_2021/Division_3/GROUPS.py
@@ -0,0 +1,8 @@
+# Problem Name: Chef and Groups
+# Problem Code: GROUPS
+
+t_cases=int(input())
+for sc in range(t_cases):
+  sdk=input().split('0')
+  sdkh=[an for an in sdk if an!='']
+  print((len(sdkh)))
